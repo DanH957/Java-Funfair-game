e045ca5892d11cd4941115eff7683b9d8c656958
Daniel Hyde <33458372+DanH957@users.noreply.github.com>
2017-11-07 15:50:45 +0000

    Sub Class

---
+++ Wall.java
@@ -0,0 +1,9 @@
+
+public class Wall implements Funfair {
+	
+	/** this code will run if the user tries to go in an invalid direction **/
+	public void enter(Player p)
+    {
+System.out.println("You cannot go in this direction");
+}
+}
